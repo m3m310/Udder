1c18f93edac20482a741ef3ddb2530f81499fbf7
mimi <s101124020@gmail.com>
2020-09-06 18:26:38 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Meme Mhd on 06/09/2020.
+//  Copyright © 2020 meme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
